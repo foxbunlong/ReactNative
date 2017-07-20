7e7dc6d2928da7afc7328650ad908ae4fb75cc39
Long Thay <thaybunlong20@yahoo.com>
2017-07-20 22:37:24 +0700

    Add Header with text and view tags

---
+++ albums/src/components/header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const Header = () => {
+
+    const { textStyle, viewStyle } = styles;
+
+    return (
+        <View style={viewStyle}>
+            <Text style={textStyle}>Albums</Text>
+        </View>
+    );
+};
+
+const styles = {
+    viewStyle: {
+        backgroundColor: '#f8f8f8'
+    },
+    textStyle: {
+        fontSize: 20
+    }
+};
+
+export default Header;
