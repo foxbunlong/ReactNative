6ae0253296709bbaec1e28396f4fa4ea6f620e13
DESKTOP-OFFICE\Long Thay <thaybunlong20@yahoo.com>
2017-07-26 14:38:22 +0700

    Parse data to object and add Card.

--- albums/src/components/AlbumList.js
+++ albums/src/components/AlbumList.js
@@ -1,18 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail'
 
 class AlbumList extends Component {
 
+    // Only use state for class based component
+    state = { albums: [] };
+
     componentWillMount() {
-        axios.get('http://rallycoding.herokuapp.com/api/music_albums').then(response => console.log(response));
+        axios.get('http://rallycoding.herokuapp.com/api/music_albums')
+            .then(response => this.setState({ albums: response.data })); // Trigger state changed
+    }
+
+    renderAlbums() {
+        return this.state.albums.map(album =>
+            <AlbumDetail key={album.title} album={album} />
+        );
     }
 
     // render to return result of jsx
     render() {
         return (
             <View>
-                <Text>Album List</Text>
+                {this.renderAlbums()}
             </View>
         );
     }
