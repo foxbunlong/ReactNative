9eff61290c564b210a5fd372c2143766cbebc623
Long Thay <thaybunlong20@yahoo.com>
2017-07-28 22:53:05 +0700

    Add common components. Add firebase and reuse app.js

---
+++ auth/src/app.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+import firebase from 'firebase';
+import { Header } from './components/common';
+
+class App extends Component {
+
+    componentWillMount() {
+        firebase.initializeApp({
+            apiKey: 'AIzaSyB1JpqsovByPR15mR-Q7-H7snCZg_Pxa-0',
+            authDomain: 'auth-54906.firebaseapp.com',
+            databaseURL: 'https://auth-54906.firebaseio.com',
+            projectId: 'auth-54906',
+            storageBucket: 'auth-54906.appspot.com',
+            messagingSenderId: '383047585862'
+        });
+    }
+
+    render() {
+        return (
+            <View>
+                <Header headerText="Authentication" />
+                <Text>App Come Here</Text>
+            </View>
+        );
+    }
+}
+
+export default App;
