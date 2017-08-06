eb295329b8d9bd6ded1e8928f68f81550a492a91
Long Thay <thaybunlong20@yahoo.com>
2017-08-06 22:28:04 +0700

    Add Reducers and common components. Simulate data in json file and load up data using redux.

--- tech_stack/src/App.js
+++ tech_stack/src/App.js
@@ -3,12 +3,17 @@ import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
 
 const App = () => {
     return (
         // Translate anything inside store to something can be used in react side
         <Provider store={createStore(reducers)}>
-            <View />
+            <View>
+                <Header headerText="Tech Stack" />
+                <LibraryList />
+            </View>
         </Provider>
     );
 }
