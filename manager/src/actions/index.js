491e91445cd5436100eff6248610ac7dcf5f2f32
DESKTOP-OFFICE\Long Thay <thaybunlong20@yahoo.com>
2017-08-11 17:31:38 +0700

    Add LOGIN_USER_SUCCESS in reducer

--- manager/src/actions/index.js
+++ manager/src/actions/index.js
@@ -1,7 +1,8 @@
 import firebase from 'firebase';
 import {
     EMAIL_CHANGED,
-    PASSWORD_CHANGED
+    PASSWORD_CHANGED,
+    LOGIN_USER_SUCCESS
 } from './Types';
 
 export const emailChanged = (text) => {
@@ -23,9 +24,25 @@ export const loginUser = ({ email, password }) => {
         firebase.auth().signInWithEmailAndPassword(email, password)
             .then(user => {
                 dispatch({
-                    type: 'LOGIN_USER_SUCCESS',
+                    type: LOGIN_USER_SUCCESS,
                     payload: user
                 });
+            })
+            .catch(() => {
+                firebase.auth().createUserWithEmailAndPassword(email, password)
+                    .then(user => {
+                        dispatch({
+                            type: LOGIN_USER_SUCCESS,
+                            payload: user
+                        });
+                    });
             });
     };
+};
+
+const loginUserSuccess = (dispatch, user) => {
+    dispatch({
+        type: LOGIN_USER_SUCCESS,
+        payload: user
+    });
 };
--- manager/src/reducers/AuthReducer.js
+++ manager/src/reducers/AuthReducer.js
@@ -1,6 +1,7 @@
 import {
     EMAIL_CHANGED,
-    PASSWORD_CHANGED
+    PASSWORD_CHANGED,
+    LOGIN_USER_SUCCESS
 } from '../actions/Types';
 
 const INITIAL_STATE = {
@@ -10,12 +11,14 @@ const INITIAL_STATE = {
 
 export default (state = INITIAL_STATE, action) => {
     console.log(action);
-    
+
     switch (action.type) {
         case EMAIL_CHANGED:
             return { ...state, email: action.payload };
         case PASSWORD_CHANGED:
             return { ...state, password: action.payload };
+        case LOGIN_USER_SUCCESS:
+            return { ...state, user: action.payload };
         default:
             return state;
     }
