597706e37b551446deef52514e313da9fb9c3112
Long Thay <thaybunlong20@yahoo.com>
2017-08-26 22:52:42 +0700

    Add Swipe2 project with manual build method

---
+++ swipe2/index.android.js
@@ -0,0 +1,6 @@
+import {
+  AppRegistry
+} from 'react-native';
+import App from './src/App';
+
+AppRegistry.registerComponent('swipe2', () => App);
---
+++ swipe2/src/Deck.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import { View, Animated } from 'react-native';
+
+class Deck extends Component {
+
+    renderCards() {
+        return this.props.data.map(item => {
+            return this.props.renderCard(item);
+        });
+    }
+
+    render() {
+        return (
+            <View>
+                {this.renderCards()}
+            </View>
+        );
+    }
+}
+
+export default Deck;
