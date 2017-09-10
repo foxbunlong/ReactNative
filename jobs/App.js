506bd8a48519b5c0050942cfdcdf2b819704c0fa
Long Thay <thaybunlong20@yahoo.com>
2017-09-10 22:19:35 +0700

    Add Jobs project and upgrade react version for FirebaseAuth project.

---
+++ jobs/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { TabNavigator } from 'react-navigation';
+
+import AuthScreen from './screens/AuthScreen';
+import WelcomeScreen from './screens/WelcomeScreen';
+
+export default class App extends React.Component {
+  render() {
+    const MainNavigator = TabNavigator({
+      welcome: { screen: WelcomeScreen },
+      auth: { screen: AuthScreen }
+    });
+
+    return (
+      <View style={styles.container}>
+        <MainNavigator />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    marginTop: StatusBar.currentHeight
+  },
+});
