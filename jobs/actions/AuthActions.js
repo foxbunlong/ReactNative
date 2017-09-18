6c87415f1eb03f1ae0625f52dcc1dfba6e6577cf
Long Thay <thaybunlong20@yahoo.com>
2017-09-18 22:40:04 +0700

    Add actions, reducers and store

---
+++ jobs/actions/AuthActions.js
@@ -0,0 +1,11 @@
+import { AsyncStorage } from 'react-native';
+import { FACEBOOK_LOGIN_SUCCESS } from './Types';
+
+// AsyncStorage.setItem('fb_token', token);
+// AsyncStorage.getItem('fb_token');
+
+export const facebookLogin = () => {
+
+    
+
+};
