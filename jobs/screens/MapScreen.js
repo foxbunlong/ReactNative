75aae725563fa930c0296c338989ccc4712c1702
Long Thay <thaybunlong20@yahoo.com>
2017-09-26 22:15:07 +0700

    Call api to fetch jobs by location. Using QS framework to build params and latlng-to-zip framework to get correct zip code.

--- jobs/screens/MapScreen.js
+++ jobs/screens/MapScreen.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
 import { View, Text, ActivityIndicator } from 'react-native';
 import { MapView } from 'expo';
+import { connect } from 'react-redux';
+import { Button } from 'react-native-elements';
+
+import * as actions from '../actions';
 
 class MapScreen extends Component {
     state = {
@@ -22,6 +26,10 @@ class MapScreen extends Component {
         this.setState({ region });
     }
 
+    onButtonPressed = () => {
+        this.props.fetchJobs(this.state.region);
+    }
+
     render() {
         if (!this.state.mapLoaded) {
             return (
@@ -37,9 +45,28 @@ class MapScreen extends Component {
                     style={{ flex: 1 }}
                     onRegionChangeComplete={this.onRegionChangeComplete}
                 />
+
+                <View style={styles.buttonContainer}>
+                    <Button
+                        large
+                        title="Search this area"
+                        backgroundColor="#009688"
+                        icon={{ name: 'search' }}
+                        onPress={this.onButtonPressed}
+                    />
+                </View>
             </View>
         );
     }
 }
 
-export default MapScreen;
+const styles = {
+    buttonContainer: {
+        position: 'absolute',
+        bottom: 20,
+        left: 0, // Stretch from left to right
+        right: 0
+    }
+}
+
+export default connect(null, actions)(MapScreen);
