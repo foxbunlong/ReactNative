97b87c584a627d421e1ac849c21fc0a580952f3d
DESKTOP-OFFICE\Long Thay <thaybunlong20@yahoo.com>
2017-09-27 13:33:37 +0700

    Add Swipe component and fill data to card

--- jobs/screens/DeckScreen.js
+++ jobs/screens/DeckScreen.js
@@ -1,18 +1,62 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { connect } from 'react-redux';
+import Swipe from '../components/Swipe';
+import { MapView } from 'expo';
+import { Card, Button } from 'react-native-elements';
 
 class DeckScreen extends Component {
+    renderCard(job) {
+        return (
+            <Card title={job.jobtitle}>
+                <View style={{ height: 300 }}>
+                    <MapView
+                        scrollEnabled={false}
+                        style={{ flex: 1 }}
+                    >
+
+                    </MapView>
+                </View>
+                <View style={styles.detailWrapper}>
+                    <Text>{job.company}</Text>
+                    <Text>{job.formattedRelativeTime}</Text>
+                </View>
+                <Text>
+                    {job.snippet.replace(/<b>/g, '').replace(/<\/b>/g, '')}
+                </Text>
+            </Card>
+        );
+    }
+
+    renderNoMoreCards() {
+        return (
+            <Card title="No more job" ></Card>
+        );
+    }
+
     render() {
-        return(
+        return (
             <View>
-                <Text>DeckScreen</Text>
-                <Text>DeckScreen</Text>
-                <Text>DeckScreen</Text>
-                <Text>DeckScreen</Text>
-                <Text>DeckScreen</Text>
+                <Swipe
+                    data={this.props.jobs}
+                    renderCard={this.renderCard}
+                    renderNoMoreCards={this.renderNoMoreCards}
+                />
             </View>
         );
     }
 }
 
-export default DeckScreen;
+const styles = {
+    detailWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        marginBottom: 20
+    }
+};
+
+function mapStateToProps({ jobs }) {
+    return { jobs: jobs.results };
+}
+
+export default connect(mapStateToProps)(DeckScreen);
