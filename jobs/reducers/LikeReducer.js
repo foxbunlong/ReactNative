754a1745f35202ca3b0005ae27ad4f837089072b
DESKTOP-OFFICE\Long Thay <thaybunlong20@yahoo.com>
2017-10-04 10:24:59 +0700

    Implement offline storage using Redux Persist. Not yet implement Migration.

--- jobs/reducers/LikeReducer.js
+++ jobs/reducers/LikeReducer.js
@@ -1,4 +1,5 @@
 import _ from 'lodash';
+import { REHYDRATE } from 'redux-persist/constants';
 import {
     LIKE_JOB,
     CLEAR_LIKED_JOBS
@@ -15,6 +16,8 @@ export default function (state = [], action) {
 
         case CLEAR_LIKED_JOBS:
             return [];
+        case REHYDRATE:
+            return action.payload.likedJobs || [];
         default:
             return state;
     }
--- jobs/store/index.js
+++ jobs/store/index.js
@@ -1,13 +1,19 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import reducers from '../reducers';
+import { persistStore, autoRehydrate } from 'redux-persist';
+import { AsyncStorage } from 'react-native';
 
 const store = createStore(
     reducers,
     {},
     compose(
-        applyMiddleware(thunk)
+        applyMiddleware(thunk),
+        autoRehydrate()
     )
 );
+
+// Every time state is changed, store all new pieces of state here, especially care about likedJobs
+persistStore(store, { storage: AsyncStorage, whitelist: ['likedJobs'] });
 
 export default store;
