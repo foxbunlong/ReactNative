20c27cd666b7d8192456d9e083be40e947bbbf89
Long Thay <long.tb@ht-tech.vn>
2023-02-06 11:44:56 +0700

    Init blogs app

---
+++ blog/App.js
@@ -0,0 +1,18 @@
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+
+import IndexScreen from "./src/screens/IndexScreen";
+
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
+  },
+  {
+    initialRouteName: "Index",
+    defaultNavigationOptions: {
+      title: "Blogs",
+    },
+  }
+);
+
+export default createAppContainer(navigator);
---
+++ blog/src/screens/IndexScreen.js
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({});
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>AAAAAA</Text>
+    </View>
+  );
+};
+
+export default IndexScreen;
