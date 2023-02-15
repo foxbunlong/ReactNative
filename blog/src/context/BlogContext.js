836ea2b0c062dfe1a34badc9ed4f8ba8f8549784
Long Thay <long.tb@ht-tech.vn>
2023-02-15 10:35:04 +0700

    Refactor blog content using useReducer

--- blog/src/context/BlogContext.js
+++ blog/src/context/BlogContext.js
@@ -1,26 +1,67 @@
-import { createContext, useState } from 'react';
+import { createContext, useReducer } from 'react';
 
 const BlogContext = createContext();
 
-export const BlogProvider = ({ children }) => {
-  const [blogPosts, setBlogPosts] = useState([
-    {
-      title: "Blog 1",
-    },
-    {
-      title: "Blog 2",
-    },
-    {
-      title: "Blog 3",
-    },
-  ]);
+const blogReducer = (state, action) => {
+  switch (action.type) {
+    case "addBlog":
+      return [...state, { title: `Blog post #${state.length + 1}` }];
 
-  const addBlog = (blogContent) => {
-    setBlogPosts([...blogPosts, {title: blogContent}]);
+    case "editBlog":
+      return state;
+
+    default:
+      return state;
+  }
+};
+
+export const BlogProvider = ({ children }) => {
+  // const [blogPosts, setBlogPosts] = useState([
+  //   {
+  //     title: "Blog 1",
+  //   },
+  //   {
+  //     title: "Blog 2",
+  //   },
+  //   {
+  //     title: "Blog 3",
+  //   },
+  // ]);
+
+  // Optional initializer function to generate the initial state
+  function init(initialValues) {
+    return [...initialValues, { title: "Blog 4 - Added" }];
+  }
+
+  const [state, dispatch] = useReducer(
+    blogReducer,
+    [
+      {
+        title: "Blog 1",
+      },
+      {
+        title: "Blog 2",
+      },
+      {
+        title: "Blog 3",
+      },
+    ],
+    init
+  );
+
+  // const addBlog = (blogContent) => {
+  //   setBlogPosts([...blogPosts, { title: blogContent }]);
+  // };
+
+  // using Reducer
+  const addBlog = () => {
+    dispatch({
+      type: "addBlog",
+    });
   };
 
   return (
-    <BlogContext.Provider value={{ data: blogPosts, addBlogPost: addBlog }}>
+    <BlogContext.Provider value={{ data: state, addBlogPost: addBlog }}>
       {children}
     </BlogContext.Provider>
   );
