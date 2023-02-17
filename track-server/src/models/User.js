f3824e6498ebb95169f40bc8a85311239b321a61
Long Thay <long.tb@ht-tech.vn>
2023-02-17 16:18:44 +0700

    Add compare password and encrypt password logic

--- track-server/src/models/User.js
+++ track-server/src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 
 // Define properties going to have in User model
 const userScheme = new mongoose.Schema({
@@ -12,5 +13,46 @@ const userScheme = new mongoose.Schema({
     required: true,
   },
 });
+
+// Pre-save hook
+userScheme.pre("save", function (next) {
+  // To access to this (user) to save, change '() =>' to 'function()'
+  // Function to call before data is saved in database
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  // 1st param: how complex the salt is
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) {
+      return next(err);
+    }
+
+    bcrypt.hash(user.password, salt, (err, encrypted) => {
+      if (err) {
+        return next(err);
+      }
+      user.password = encrypted;
+      next();
+    });
+  });
+});
+
+userScheme.methods.comparePassword = function (inputPassword) {
+  return new Promise((resolve, reject) => {
+    bcrypt.compare(inputPassword, this.password, (err, isMatched) => {
+      if (err) {
+        return reject(err);
+      }
+
+      if (!isMatched) {
+        return reject(false);
+      }
+
+      return resolve(true);
+    });
+  });
+};
 
 mongoose.model("User", userScheme);
--- track-server/src/routes/authRoutes.js
+++ track-server/src/routes/authRoutes.js
@@ -14,7 +14,29 @@ router.post("/signup", async (req, res) => {
     const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
     res.send({ token });
   } catch (error) {
-    return res.status(422).send(error.message);
+    return res.status(422).send({ message: error.message });
+  }
+});
+
+router.post("/signin", async (req, res) => {
+  const { email, password } = req.body;
+  if (!email || !password) {
+    return res
+      .status(422)
+      .send({ message: "Must provide username and password" });
+  }
+
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(422).send({ message: "Invalid email or password" });
+  }
+
+  try {
+    await user.comparePassword(password);
+    const token = jwt.sign({ userId: user._id }, "MY_SECRET_KEY");
+    res.send({ token });
+  } catch (error) {
+    return res.status(422).send({ message: "Invalid email or password" });
   }
 });
 
